29bdad22e9edb006b45f5f03dfa6cbcf746bde0e
Aline Yumi Takata <atakata@c1r4p3.42sp.org.br>
2026-09-17 18:53:35 -0300

    python 00 e parte do 01

---
+++ python01/ex0/ft_garden_intro.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def garden_intro() -> None:
+    name = "Rose"
+    height = 25
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print()
+    print ("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    garden_intro()
---
+++ python01/ex1/ft_garden_data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+class Plant:
+    def show(self) -> None:
+        
+
+if __name__ == "__main__":
+    print("=== Garden Plant Registry ===")
