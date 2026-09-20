f3e140e10b588aa3288c5458e41aa49b94dae4fd
Aline Takata <254481329+lineeyumi@users.noreply.github.com>
2026-09-19 22:32:09 -0300

    ex 5

--- python01/ex5/ft_plant_types.py
+++ python01/ex5/ft_plant_types.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+
+class Plant():
+    name: str
+    height: float
+    age: int
+
+    def __init__(
+        self,
+        name: str,
+        height: float,
+        age: int
+    ) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show(self):
+        print(f"=== {self.__class__.__name__}")
+        print(f"{self.name}: {self.height:.1f}cm, {self.age} days old")
+
+    class Flower(Plant):
+        def __init__(
+            self,
+            name: str,
+            height: float,
+            age: int,
+            color: str
+        ) -> None:
+            super().__init__(name, height, age)
+
+        def bloom(self) -> None:
+            bloom_flower: str
+            
+
+if __name__ == "__main__":
+    print("=== Garden Plant Types ===")
+    rose = Plant("Rose", 15, 10, "Flower")
+    oak = Plant("Oak", 15, 10, "Tree")
+    tomato = Plant("Tomato", 15, 10, "Vegetable")
